0a36499a7b3eedac4686fa050666c52d235cd5e1
chiedo07 <chiedoluiz@gmail.com>
2023-01-07 20:43:47 +0000

    cart design

--- inc.js
+++ inc.js
@@ -1,6 +1,8 @@
 const plus = document.querySelector("#BtInc"),
 subs = document.querySelector("#BtDec"),
 num = document.querySelector("#val");
+valz = document.querySelector(".value3");
+ 
  
 let x = 0;
 
@@ -12,17 +14,26 @@ plus.addEventListener("click" , () => {
    
    console.log(x);
 
+   b = x * 125;
+   valz.innerHTML = new Intl.NumberFormat('en-US',options).format(b);
+   console.log(val);
+
+
 });
 
 subs.addEventListener("click" , () => {
     
-   if(x > 0){
+   if(x > 1){
       x--;
    
      num.innerText = x;
 
       console.log(x);
        
+      b = x * 125;
+      valz.innerHTML = new Intl.NumberFormat('en-US',options).format(b);
+      console.log(val);
+  
 
    }
 });
@@ -31,6 +42,8 @@ subs.addEventListener("click" , () => {
 const add = document.querySelector("#btn-inc"),
 sub = document.querySelector("#btn-dec"),
 value = document.querySelector("#cnt");
+val = document.querySelector(".value");
+let options = { style: 'currency', currency: 'USD' };
  
 let a = 0;
 
@@ -41,21 +54,32 @@ add.addEventListener("click" , () => {
    value.innerText = x;
    
    console.log(x);
+     
+   b = x * 125;
+    val.innerHTML = new Intl.NumberFormat('en-US',options).format(b);
+    console.log(val);
 
 });
 
 sub.addEventListener("click" , () => {
     
-   if(x > 0){
+   if(x > 1){
       x--;
    
      value.innerText = x;
 
       console.log(x);
-       
+      b = x * 125;
+    val.innerHTML = new Intl.NumberFormat('en-US',options).format(b);
+    console.log(val);
 
    }
+   
+  
 });
 
 
 // Adding To Cart
+function AddToCart(){
+   document.createElement('div');
+}
